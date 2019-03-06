df84eb3645a26011d2e6845fd02a0d523724fd6c
aleixcortines <aleixcortines@gmail.com>
2019-03-06 14:45:47 +0100

    json done

---
+++ project/data_json.js
@@ -0,0 +1,159 @@
+var json_data = {
+    "items": [
+        {
+            "name_hackaton": "Dublin Hack Party",
+            "map_location": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8018.916358018328!2d-6.225597261865575!3d53.306763978607556!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4867093039c10e1b%3A0xc2965e50c5f8d73e!2sEngineering+Building%2C+UCD+Engineering+%26+Materials+Science+Centre%2C+University+College+Dublin%2C+Stillorgan+Rd%2C+Belfield%2C+Dublin+4%2C+Irlanda!5e0!3m2!1sca!2ses!4v1551264216829",
+            "university_location": "UCD Dublin Engineering",
+            "description":"This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+            "matches": [
+                {
+                    "id": 0,
+                    "team_one": "Youth Team",
+                    "image_one": "https://i.ibb.co/Bt9HGk7/team1.jpg",
+                    "team_two": "Ubiqum",
+                    "image_two": "https://i.ibb.co/1fkd2RW/team2.jpg"
+                },
+                {
+                    "id": 1,
+                    "team_one": "Axxio Team",
+                    "image_one": "https://i.ibb.co/Dk5DSSV/team3.png",
+                    "team_two": "TurkHack",
+                    "image_two": "https://i.ibb.co/R7zz0WV/team4.jpg"
+                },
+                {
+                    "id": 2,
+                    "team_one": "UmbrellaCorp",
+                    "image_one": "https://i.ibb.co/tXGv3MW/team5.jpg",
+                    "team_two": "Aliens",
+                    "image_two": "https://i.ibb.co/F0ShWwQ/team6.jpg"
+                }
+            ]
+},
+        {
+            "name_hackaton": "Toronto Hack",
+            "map_location": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11547.054925835526!2d-79.4031089212274!3d43.65308379133684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b34b8a1a7d307%3A0xe36b1ff6609c7d2c!2sUniversity+of+Toronto+Faculty+of+Applied+Science+and+Engineering!5e0!3m2!1sca!2ses!4v1551713706749",
+            "university_location": "Faculty of Engineering",
+            "description":"This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+            "matches": [
+                {
+                    "id": 0,
+                    "team_one": "Youth Team",
+                    "image_one": "https://i.ibb.co/Bt9HGk7/team1.jpg",
+                    "team_two": "Ubiqum",
+                    "image_two": "https://i.ibb.co/1fkd2RW/team2.jpg"
+                },
+                {
+                    "id": 1,
+                    "team_one": "Axxio Team",
+                    "image_one": "https://i.ibb.co/Dk5DSSV/team3.png",
+                    "team_two": "TurkHack",
+                    "image_two": "https://i.ibb.co/R7zz0WV/team4.jpg"
+                },
+                {
+                    "id": 2,
+                    "team_one": "UmbrellaCorp",
+                    "image_one": "https://i.ibb.co/tXGv3MW/team5.jpg",
+                    "team_two": "Aliens",
+                    "image_two": "https://i.ibb.co/F0ShWwQ/team6.jpg"
+                }
+            ]
+},
+        {
+            "name_hackaton": "Tokyo Hack Party",
+            "map_location": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25920.59244780854!2d139.74078610791247!3d35.6997951834304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188c31cd6b36fb%3A0xcb7e467e917c3672!2sFaculty+of+Engineering+Building+2%2C+The+University+of+Tokyo!5e0!3m2!1sca!2ses!4v1551714075811",
+            "university_location": "University of Tokyo",
+            "description":"This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+            "matches": [
+                {
+                    "id": 0,
+                    "team_one": "Youth Team",
+                    "image_one": "https://i.ibb.co/Bt9HGk7/team1.jpg",
+                    "team_two": "Ubiqum",
+                    "image_two": "https://i.ibb.co/1fkd2RW/team2.jpg"
+                },
+                {
+                    "id": 1,
+                    "team_one": "Axxio Team",
+                    "image_one": "https://i.ibb.co/Dk5DSSV/team3.png",
+                    "team_two": "TurkHack",
+                    "image_two": "https://i.ibb.co/R7zz0WV/team4.jpg"
+                },
+                {
+                    "id": 2,
+                    "team_one": "UmbrellaCorp",
+                    "image_one": "https://i.ibb.co/tXGv3MW/team5.jpg",
+                    "team_two": "Aliens",
+                    "image_two": "https://i.ibb.co/F0ShWwQ/team6.jpg"
+                }
+            ]
+},
+        {
+            "name_hackaton": "London Hackaton",
+            "map_location": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2483.1281869014383!2d-0.11768112562070901!3d51.51086421832562!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487604ca706f98bd%3A0xdc543f6bcca79a07!2sCourtauld+Institute+of+Art!5e0!3m2!1sca!2ses!4v1551789528547",
+            "university_location": "King's College London",
+            "description":"This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+            "matches": [
+                {
+                    "id": 0,
+                    "team_one": "Youth Team",
+                    "image_one": "https://i.ibb.co/Bt9HGk7/team1.jpg",
+                    "team_two": "Ubiqum",
+                    "image_two": "https://i.ibb.co/1fkd2RW/team2.jpg"
+                },
+                {
+                    "id": 1,
+                    "team_one": "Axxio Team",
+                    "image_one": "https://i.ibb.co/Dk5DSSV/team3.png",
+                    "team_two": "TurkHack",
+                    "image_two": "https://i.ibb.co/R7zz0WV/team4.jpg"
+                },
+                {
+                    "id": 2,
+                    "team_one": "UmbrellaCorp",
+                    "image_one": "https://i.ibb.co/tXGv3MW/team5.jpg",
+                    "team_two": "Aliens",
+                    "image_two": "https://i.ibb.co/F0ShWwQ/team6.jpg"
+                }
+            ]
+}
+],
+    "events": [
+        {
+            "city":"DUBLIN",
+            "country": "IRELAND",
+            "date": "5 MAR 2019",
+            "image":"https://i.ibb.co/c3b1Z9h/image-dublin.png",
+            "name":"Dublin Hack Party",
+            "description": "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+        },
+        {
+            "city":"TORONTO",
+            "country": "CANADA",
+            "date": "15 MAR 2019",
+            "image":"https://i.ibb.co/rHWXbZJ/image-toronto.jpg",
+            "name":"Toronto Hack",
+            "description": "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+             
+        },
+        {
+            "city":"TOKYO",
+            "country": "JAPAN",
+            "date": "6 MAY 2019",
+            "image":"https://i.ibb.co/7Y6Vw3N/image-tokyo.jpg",
+            "name":"Tokyo Hack Party",
+            "description": "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+            
+            
+        },
+        {
+            "city":"LONDON",
+            "country": "UK",
+            "date": "10 MAY 2019",
+            "image":"https://i.ibb.co/T0qbD4B/image-london.jpg",
+            "name":"London Hackaton",
+            "description": "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+            
+            
+        }
+    ]
+}
